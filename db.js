1200b4b600ca8a9d3e21dec90feefd9f05faea5b
Juhan Oskar Hennoste <juhan.hennoste@ut.ee>
2021-12-08 21:01:15 +0200

    Improve async endpoints' error handling

--- db.js
+++ db.js
@@ -2,18 +2,22 @@ const pg = require('pg')
 
 const pool = new pg.Pool()
 
+/**
+ * Run query and return all results as array
+ */
 async function queryMultiple(query, params) {
     return (await pool.query(query, params)).rows
 }
 
+/**
+ * Run query and return the single result. If query gives multiple or no results, throw error
+ */
 async function querySingle(query, params) {
     const rows = (await pool.query(query, params)).rows
-    if (rows.length === 0) {
-        return null
-    } else if (rows.length === 1) {
+    if (rows.length === 1) {
         return rows[0]
     } else {
-        throw new Error(`Expected at most one result for query, got ${rows.length}`)
+        throw new Error(`Expected exactly 1 result for query, got ${rows.length}`)
     }
 }
 
--- routes.js
+++ routes.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler')
 const db = require('./db')
 
 const routes = express.Router()
 
-routes.get('/', async (req, res) => {
+routes.get('/', asyncHandler(async (req, res) => {
     const posts = await db.queryMultiple('SELECT * FROM posts')
     res.render('posts', { posts })
-})
+}))
 
-routes.get('/singlepost/:id', async (req, res) => {
+routes.get('/singlepost/:id', asyncHandler(async (req, res) => {
     const post = await db.querySingle('SELECT * FROM posts WHERE id = $1', [req.params.id])
     res.render('singlepost', { post })
-})
+}))
 
-routes.get('/addnewpost', async (req, res) => res.render('addnewpost'))
+routes.get('/addnewpost', asyncHandler(async (req, res) => res.render('addnewpost')))
 
 module.exports = routes
